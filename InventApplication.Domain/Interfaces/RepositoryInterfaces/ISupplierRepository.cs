7186843b123e4833659252ff0086baafa77c3a1e
Preethamcgowda <preetham.c@excelindia.com>
2023-04-24 21:28:26 +0530

    Supplier Module codes

---
+++ InventApplication.Domain/Interfaces/RepositoryInterfaces/ISupplierRepository.cs
@@ -0,0 +1,20 @@
+﻿using InventApplication.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventApplication.Domain.Interfaces.RepositoryInterfaces
+{
+    public interface ISupplierRepository
+    {
+        public Task<bool> AddSupplier(Supplier supplier);
+        public Task<Supplier> GetSupplierByIdAsync(int supplierid);
+        public Task<IEnumerable<Supplier>> GetAllSupplierAsync();
+        public Task<bool> UpdateSupplier(Supplier supplierUpdate, int supplierid);
+        public Task<bool> DeleteSupplier(int supplierid);
+
+
+    }
+}
---
+++ InventApplication.Infrastructure/Common/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using InventApplication.Domain.Interfaces.RepositoryInterfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventApplication.Infrastructure.Common
+{
+    public class DataAccess : IDataAccess
+    {
+        public readonly IConfiguration _configuration;
+        public DataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetConnectionString()
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+
+            return _configuration.GetSection("ConnectionStrings").GetSection("DBConnectionString").Value;
+
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+    }
+}
